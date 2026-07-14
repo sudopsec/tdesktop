a0f2f5bb4f94ee4e1d79d8be4a61e05bb34e9b61
ff <277307031+sudopsec@users.noreply.github.com>
2026-07-14 12:30:20 +0600

    Update built-in DC options to use localhost

--- Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
+++ Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
@@ -29,52 +29,52 @@ struct BuiltInDc {
 };
 
 const BuiltInDc kBuiltInDcs[] = {
-	{ 1, "149.154.175.50" , 443 },
-	{ 2, "149.154.167.51" , 443 },
-	{ 2, "95.161.76.100"  , 443 },
-	{ 3, "149.154.175.100", 443 },
-	{ 4, "149.154.167.91" , 443 },
-	{ 5, "149.154.171.5"  , 443 },
+	{ 1, "127.0.0.1" , 4430 },
+	{ 2, "127.0.0.1" , 4430 },
+	{ 2, "127.0.0.1" , 4430 },
+	{ 3, ""127.0.0.1" , 4430 },
+	{ 4, "127.0.0.1" , 4430 },
+	{ 5, "127.0.0.1" , 4430 },
 };
 
 const BuiltInDc kBuiltInDcsIPv6[] = {
-	{ 1, "2001:0b28:f23d:f001:0000:0000:0000:000a", 443 },
-	{ 2, "2001:067c:04e8:f002:0000:0000:0000:000a", 443 },
-	{ 3, "2001:0b28:f23d:f003:0000:0000:0000:000a", 443 },
-	{ 4, "2001:067c:04e8:f004:0000:0000:0000:000a", 443 },
-	{ 5, "2001:0b28:f23f:f005:0000:0000:0000:000a", 443 },
+	{ 1, "127.0.0.1" , 4430 },
+	{ 2, "127.0.0.1" , 4430 },
+	{ 3, "127.0.0.1" , 4430 },
+	{ 4, "127.0.0.1" , 4430 },
+	{ 5, "127.0.0.1" , 4430 },
 };
 
 const BuiltInDc kBuiltInDcsTest[] = {
-	{ 1, "149.154.175.10" , 443 },
-	{ 2, "149.154.167.40" , 443 },
-	{ 3, "149.154.175.117", 443 }
+	{ 1, "127.0.0.1" , 4430 },
+	{ 2, "127.0.0.1" , 4430 },
+	{ 3, "127.0.0.1" , 4430 }
 };
 
 const BuiltInDc kBuiltInDcsIPv6Test[] = {
-	{ 1, "2001:0b28:f23d:f001:0000:0000:0000:000e", 443 },
-	{ 2, "2001:067c:04e8:f002:0000:0000:0000:000e", 443 },
-	{ 3, "2001:0b28:f23d:f003:0000:0000:0000:000e", 443 }
+	{ 1, "127.0.0.1" , 4430 },
+	{ 2, "127.0.0.1" , 4430 },
+	{ 3, "127.0.0.1" , 4430 }
 };
 
 const char *kTestPublicRSAKeys[] = { "\
 -----BEGIN RSA PUBLIC KEY-----\n\
-MIIBCgKCAQEAyMEdY1aR+sCR3ZSJrtztKTKqigvO/vBfqACJLZtS7QMgCGXJ6XIR\n\
-yy7mx66W0/sOFa7/1mAZtEoIokDP3ShoqF4fVNb6XeqgQfaUHd8wJpDWHcR2OFwv\n\
-plUUI1PLTktZ9uW2WE23b+ixNwJjJGwBDJPQEQFBE+vfmH0JP503wr5INS1poWg/\n\
-j25sIWeYPHYeOrFp/eXaqhISP6G+q2IeTaWTXpwZj4LzXq5YOpk4bYEQ6mvRq7D1\n\
-aHWfYmlEGepfaYR8Q0YqvvhYtMte3ITnuSJs171+GDqpdKcSwHnd6FudwGO4pcCO\n\
-j4WcDuXc2CTHgH8gFTNhp/Y8/SpDOhvn9QIDAQAB\n\
+MIIBCgKCAQEAzYCJV3NxLT3DKw5IfA7UgkbCeJXUnMNiFZvCQYVhsHHP91J05GjK\n\
+URCAbTPALJYBHLltWxFSSX0TOLq+xKQA9n7t2O6aJpdRFWQHvHZsklweJxkZVIRh\n\
+kVh66h54wylZDIf4NN8Apx1BkANl3br1Zj/+ODjrsR2CFfZ7JsjNESyCi1Am6msu\n\
+zJLmx1CRFyaybNM4eTi5IxDzeFBHlOHkbxxSj3qh+htAWy4iR6zbSN5p8T+Sof3T\n\
+LWUzOfq4ajzOZt2l2fL6wAJYSTGd1GibjIEeegbQa9m5ZfnONTQhMUDvJzIyxd+b\n\
+y9+YfiabibNBTLkyugmkeV6Tz4RsCXI4tQIDAQAB\n\
 -----END RSA PUBLIC KEY-----" };
 
 const char *kPublicRSAKeys[] = { "\
 -----BEGIN RSA PUBLIC KEY-----\n\
-MIIBCgKCAQEA6LszBcC1LGzyr992NzE0ieY+BSaOW622Aa9Bd4ZHLl+TuFQ4lo4g\n\
-5nKaMBwK/BIb9xUfg0Q29/2mgIR6Zr9krM7HjuIcCzFvDtr+L0GQjae9H0pRB2OO\n\
-62cECs5HKhT5DZ98K33vmWiLowc621dQuwKWSQKjWf50XYFw42h21P2KXUGyp2y/\n\
-+aEyZ+uVgLLQbRA1dEjSDZ2iGRy12Mk5gpYc397aYp438fsJoHIgJ2lgMv5h7WY9\n\
-t6N/byY9Nw9p21Og3AoXSL2q/2IJ1WRUhebgAdGVMlV1fkuOQoEzR7EdpqtQD9Cs\n\
-5+bfo3Nhmcyvk5ftB0WkJ9z6bNZ7yxrP8wIDAQAB\n\
+MIIBCgKCAQEAzYCJV3NxLT3DKw5IfA7UgkbCeJXUnMNiFZvCQYVhsHHP91J05GjK\n\
+URCAbTPALJYBHLltWxFSSX0TOLq+xKQA9n7t2O6aJpdRFWQHvHZsklweJxkZVIRh\n\
+kVh66h54wylZDIf4NN8Apx1BkANl3br1Zj/+ODjrsR2CFfZ7JsjNESyCi1Am6msu\n\
+zJLmx1CRFyaybNM4eTi5IxDzeFBHlOHkbxxSj3qh+htAWy4iR6zbSN5p8T+Sof3T\n\
+LWUzOfq4ajzOZt2l2fL6wAJYSTGd1GibjIEeegbQa9m5ZfnONTQhMUDvJzIyxd+b\n\
+y9+YfiabibNBTLkyugmkeV6Tz4RsCXI4tQIDAQAB\n\
 -----END RSA PUBLIC KEY-----" };
 
 } // namespace
